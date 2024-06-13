ce853b57c67e849d38e5b2af2fd0f334af9bc7bd
Mehmet Fidan <mehmetfidan889@gmail.com>
2024-06-13 23:13:03 +0300

    footer

--- components/Newslatter.vue
+++ components/Newslatter.vue
@@ -1,41 +1,351 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-6 newsfirst">
-        <h1>Nature is the best artist.</h1>
-      </div>
-      <div class="col-md-6">
-        <input type="text" placeholder="Email" class="email-input" />
-        <button>Contact Us</button>
-        <p>
-          * We will never share your email address & you can opt out at any
-          time, we promise.
-        </p>
+  <footer id="footer1">
+    <!--.sect20-->
+    <div class="sect30">
+      <div class="standard_center1">
+        <div class="grid">
+          <div class="s s1">
+            <div class="bos"></div>
+          </div>
+          <div class="s s2">
+            <form id="form_mail_abonelik" action="" method="POST">
+              <input type="hidden" name="router" value="mail_abonelik" />
+              <div class="grid2">
+                <div class="grid2s s1">
+                  <input type="email" name="email" placeholder="email" />
+                </div>
+                <div class="grid2s s2">
+                  <button>Contact Us</button>
+                  <img class="loading" src="" alt="" />
+                </div>
+              </div>
+            </form>
+            <div class="text1">
+              We will never share your email address & you can opt out at any
+              time, we promise.
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+    <!--.sect30-->
+    <div class="sect40">
+      <div class="standard_center1">
+        <div class="grid">
+          <div class="s s1">
+            <a href="#">
+              <img src="~/assets/img/natura_onecoat.jpg" alt="" />
+            </a>
+          </div>
+          <div class="s s2">
+            <div class="grid2">
+              <div class="grid2s">
+                <div class="navtitle"></div>
+                <div class="list">
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                </div>
+              </div>
+              <div class="grid2s">
+                <div class="navtitle"></div>
+                <div class="list">
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                </div>
+              </div>
+              <div class="grid2s">
+                <div class="navtitle"></div>
+                <div class="list">
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                  <div><a href="#">We are?</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--.sect40-->
+    <div class="standard_center1">
+      <div class="sect50">
+        <div class="text1">
+          We will never share your email address & you can opt out at any time,
+          we promise.
+        </div>
+        <div class="icons">
+          <a
+            href="https://www.instagram.com/kollektmagofficial/"
+            target="_blank"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              style="width: 50px; height: 50px"
+            >
+              <path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              /></svg
+          ></a>
+          <a
+            href="https://www.youtube.com/channel/UCRrxYuHiH-6KvvoYuN_VKxA"
+            target="_blank"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              style="width: 50px; height: 50px"
+            >
+              <path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              /></svg
+          ></a>
+          <a href="mailto:info@kollektmag.com" target="_blank"
+            ><i class="fa-regular fa-envelope"></i
+          ></a>
+          <a
+            href="https://www.linkedin.com/company/kollekt-mag-official/"
+            target="_blank"
+            ><i class="fa-brands fa-linkedin-in"></i
+          ></a>
+          <a
+            href="https://twitter.com/i/flow/login?redirect_after_login=%2FKollektMag"
+            target="_blank"
+            ><i class="fa-brands fa-twitter"></i
+          ></a>
+          <a href="https://www.facebook.com/kollektmedya" target="_blank"
+            ><i class="fa-brands fa-facebook"></i
+          ></a>
+          <!--https://twitter.com/i/flow/login?redirect_after_login=%2FKollektMag-->
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-.newsfirst {
-  background: #272727 0% 0% no-repeat padding-box;
+a {
+  text-decoration: none;
+  color: black;
 }
-.newsfirst h1 {
-  text-align: left;
-  font-size: 37px;
-  font-family: 'Avenir Book';
-  letter-spacing: 0px;
-  color: #ffffff;
-  opacity: 1;
+
+#footer1 {
+  margin: 70px 0 0 0;
 }
-.email-input {
+#footer1 .sect30 {
+  margin: 40px 0 0 0;
+}
+#footer1 .sect30 .grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+}
+#footer1 .sect30 .grid .s {
+}
+#footer1 .sect30 .grid .s.s1 {
+}
+#footer1 .sect30 .grid .s.s1 .bos {
+  height: 50px;
+  background: #272727;
+}
+#footer1 .sect30 .grid .s.s2 {
+}
+#footer1 .sect30 .grid .s.s2 form {
+}
+#footer1 .sect30 .grid .s.s2 form .grid2 {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+}
+#footer1 .sect30 .grid .s.s2 form .grid2s {
+}
+#footer1 .sect30 .grid .s.s2 form .grid2s.s1 {
+}
+#footer1 .sect30 .grid .s.s2 form .grid2s.s1 input {
+  display: block;
+  width: 100%;
+  height: 50px;
+  padding: 0 15px;
+  background: #fff;
+  box-sizing: border-box;
   border: 1px solid #272727;
 }
-button {
-  background: #272727 0% 0% no-repeat padding-box;
-  font-size: 12px;
-  letter-spacing: 0px;
-  color: #ffffff;
-  opacity: 1;
+#footer1 .sect30 .grid .s.s2 form .grid2s.s2 {
+  text-align: center;
+}
+#footer1 .sect30 .grid .s.s2 form .grid2s.s2 button {
+  display: block;
+  width: 100%;
+  height: 50px;
+  background: #272727;
+  border: none;
+  text-align: center;
+  color: #fff;
+  font-family: 'poppins_light';
+  font-size: 1rem;
+}
+#footer1 .sect30 .grid .s.s2 form .grid2s.s2 img.loading {
+  display: none;
+  width: auto;
+  height: 30px;
+  margin: 10px;
+}
+#footer1 .sect30 .grid .s.s2 .text1 {
+  margin: 5px 0 0 0;
+  font-size: 0.9rem;
+}
+
+#footer1 .sect40 {
+  margin: 50px 0 0 0;
+}
+#footer1 .sect40 .grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+}
+#footer1 .sect40 .grid .s {
+}
+#footer1 .sect40 .grid .s.s1 {
+  align-self: end;
+}
+#footer1 .sect40 .grid .s.s1 a {
+}
+#footer1 .sect40 .grid .s.s1 a img {
+  width: 200px;
+  height: auto;
+}
+#footer1 .sect40 .grid .s.s2 {
+  padding: 0 0 40px 0;
+}
+#footer1 .sect40 .grid2 {
+  max-width: 100%;
+  display: inline-grid;
+  grid-template-columns: repeat(3, auto);
+  grid-gap: 30px;
+}
+#footer1 .sect40 .grid2s {
+}
+#footer1 .sect40 .grid2s .navtitle {
+  margin: 0 0 10px 0;
+  font-family: 'avenir_medium';
+  font-size: 1.2rem;
+}
+#footer1 .sect40 .grid2s .list {
+}
+#footer1 .sect40 .grid2s .list div {
+}
+#footer1 .sect40 .grid2s .list div a {
+}
+
+#footer1 .sect50 {
+  margin: 50px 0 0 0;
+  padding: 40px 0 40px 0;
+  color: #000;
+  text-align: center;
+}
+#footer1 .sect50 a {
+  color: #fff;
+}
+#footer1 .sect50 .text1 {
+  font-size: 0.9rem;
+}
+#footer1 .sect50 .icons {
+  margin: 10px 0 0 0;
+  font-size: 1.5rem;
+}
+#footer1 .sect50 .icons a {
+  margin: 0 5px;
+}
+@media only screen and (max-width: 1200px) {
+  #footer1 .sect20 .grid .s.s1 {
+    grid-area: s1;
+    padding: 0;
+  }
+  #footer1 .sect20 .grid .s.s2 {
+    grid-area: s2;
+  }
+  #footer1 .sect20 .grid .s.s3 {
+    grid-area: s3;
+    padding: 0;
+  }
+  #footer1 .sect20 .grid .s.s4 {
+    grid-area: s4;
+  }
+  #footer1 .sect20 .grid .s.s5 {
+    grid-area: s5;
+    padding: 0;
+  }
+  #footer1 .sect20 .grid .s.s6 {
+    grid-area: s6;
+  }
+
+  #footer1 .sect30 .grid {
+    grid-template-columns: 1fr;
+    grid-gap: 10px;
+  }
+  #footer1 .sect30 .grid .s.s1 .bos {
+    height: 10px;
+  }
+
+  #footer1 .sect40 {
+    margin: 50px 0 0 0;
+  }
+  #footer1 .sect40 .grid {
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+  }
+  #footer1 .sect40 .grid .s.s2 {
+    padding: 0;
+  }
+  #footer1 .sect40 .grid2 {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+  }
+}
+@media only screen and (max-width: 900px) {
+  #footer1 .sect20 {
+    font-size: 1.1rem;
+  }
+  #footer1 .sect20 .grid {
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
+  }
+  #footer1 .sect20 .grid .s .iconlu1 {
+    font-size: 1.2rem;
+  }
+  #footer1 .sect20 .grid .s .iconlu1 table tr td:nth-of-type(2) {
+    padding: 0 0 0 5px;
+  }
+
+  #footer1 .sect40 .grid2 {
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 30px;
+    text-align: center;
+  }
+  #footer1 .sect40 .grid .s.s1 {
+    text-align: center;
+  }
+  #footer1 .sect40 .grid .s.s1 a img {
+    width: 150px;
+  }
+  #footer1 .sect40 .grid2s .navtitle {
+    margin: 0 0 5px 0;
+  }
+}
+@media only screen and (max-width: 700px) {
+  #footer1 .sect20 .grid {
+    grid-template-columns: 1fr;
+    grid-row-gap: 5px;
+    grid-template-areas: 's1' 's3' 's5' 's2' 's4' 's6';
+  }
+  #footer1 .sect20 .grid .s.s2 {
+    padding: 30px 0 0 0;
+  }
+}
+@media only screen and (max-width: 600px) {
+  #footer1 .sect30 .grid .s.s2 form .grid2 {
+    grid-template-columns: 2fr 1fr;
+  }
 }
 </style>
