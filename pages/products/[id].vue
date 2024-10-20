2dd18127bea706f98ffe87fb0317c435c3dd1ea8
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-10-21 00:34:11 +0300

    update product detail 007

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -50,26 +50,46 @@
 								<div class="colors row">
 									<h1 class="pftitle clrs">COLOURS</h1>
 									<div class="col-md-12">
-										<!-- Selectbox Container -->
-										<div class="color-selectbox">
-											<select v-model="selectedColor">
-												<option
-													v-for="(
-														image, index
-													) in images"
-													:key="index"
-													:value="image"
+										<div class="custom-select">
+											<!-- Seçenekleri liste olarak gösteriyoruz -->
+											<div
+												class="selected-option"
+												@click="toggleDropdown"
+											>
+												<span v-if="selectedOption">
+													{{ getSelectedOptionText }}
+													<img
+														:src="
+															getSelectedOptionImage
+														"
+														class="img-flag"
+														alt="selected option image"
+													/>
+												</span>
+												<span v-else
+													>Select an option</span
 												>
-													{{ image.title }}
-												</option>
-											</select>
-											<!-- Seçilen resim sağda görüntülenecek -->
-											<div class="selected-image">
-												<img
-													:src="selectedColor.src"
-													:alt="selectedColor.title"
-												/>
 											</div>
+
+											<ul
+												v-if="dropdownVisible"
+												class="options-list"
+											>
+												<li
+													v-for="option in options"
+													:key="option.value"
+													@click="
+														selectOption(option)
+													"
+												>
+													{{ option.text }}
+													<img
+														:src="option.src"
+														class="img-flag"
+														alt="option image"
+													/>
+												</li>
+											</ul>
 										</div>
 									</div>
 								</div>
@@ -195,6 +215,84 @@ const error = ref(null)
 const directusBaseUrl = useRuntimeConfig().public.directusApiUrl
 const showDetails = ref(false)
 const selectedColor = ref(images[0]) // Seçili renk başlangıçta ilk renk
+
+const dropdownVisible = false // Dropdown'un açık olup olmadığını kontrol eder
+const selectedOption = null // Seçilen opsiyonu tutar
+
+const options = [
+	{
+		src: '/colors_new/clear.png',
+		text: 'CLEAR',
+		value: 'option-1',
+	},
+	{
+		src: '/colors_new/natural_white.png',
+		text: 'NATURAL WHITE',
+		value: 'option-2',
+	},
+	{
+		src: '/colors_new/white.png',
+		text: 'WHITE',
+		value: 'option-3',
+	},
+	{
+		src: '/colors_new/natural_mist.png',
+		text: 'NATURAL MIST',
+		value: 'option-4',
+	},
+	{
+		src: '/colors_new/dark_oak.png',
+		text: 'DARK OAK',
+		value: 'option-5',
+	},
+	{
+		src: '/colors_new/black.png',
+		text: 'BLACK',
+		value: 'option-6',
+	},
+	{
+		src: '/colors_new/walnut.png',
+		text: 'WALNUT',
+		value: 'option-7',
+	},
+	{
+		src: '/colors_new/chocolate_brown.png',
+		text: 'CHOCOLATE BROWN',
+		value: 'option-8',
+	},
+	{
+		src: '/colors_new/gray.png',
+		text: 'GRAY',
+		value: 'option-9',
+	},
+	{
+		src: '/colors_new/charcoal.png',
+		text: 'CHARCOAL',
+		value: 'option-10',
+	},
+	{
+		src: '/colors_new/soft_white.png',
+		text: 'SOFT WHİTE',
+		value: 'option-11',
+	},
+]
+
+const getSelectedOptionImage = () => {
+	const option = options.find((opt) => opt.value === selectedOption)
+	return option ? option.src : ''
+}
+getSelectedOptionText = () => {
+	const option = options.find((opt) => opt.value === selectedOption)
+	return option ? option.text : ''
+}
+
+toggleDropdown = () => {
+	dropdownVisible = !dropdownVisible
+}
+selectOption = (option) => {
+	selectedOption = option.value
+	dropdownVisible = false // Seçimden sonra dropdown'ı kapat
+}
 
 const fetchProduct = async (productId) => {
 	try {
@@ -439,5 +537,51 @@ definePageMeta({
 }
 .bl {
 	border-left: 9px solid white !important;
+}
+
+.custom-select {
+	position: relative;
+	display: inline-block;
+	width: 250px;
+}
+
+.selected-option {
+	border: 1px solid #ccc;
+	padding: 10px;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: space-between; /* Boşlukları düzgün dağıtmak için */
+}
+
+.img-flag {
+	width: 20px;
+	height: 20px;
+	margin-left: 10px;
+	flex-shrink: 0; /* Resmin küçülmesini önler */
+}
+
+.options-list {
+	position: absolute;
+	width: 100%;
+	border: 1px solid #ccc;
+	background-color: white;
+	list-style-type: none;
+	padding: 0;
+	margin: 0;
+	z-index: 1;
+}
+
+.options-list li {
+	padding: 10px;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: space-between; /* Boşlukları düzgün dağıtmak için */
+	white-space: nowrap; /* Satırların alt alta geçmesini engeller */
+}
+
+.options-list li:hover {
+	background-color: #f0f0f0;
 }
 </style>
