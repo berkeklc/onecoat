9c43d6ba3538e30b3b1344331c99486269249bb5
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-10-24 00:44:23 +0300

    fixed

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,66 +1,146 @@
 <template>
-	<Logo />
 	<div>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 headtitle">
-					<h1>PRODUCTS <span class="of">of</span> NATURA</h1>
-					<p>
-						Only one coat protection of all kinds of interior wooden
-						surfaces for flooring and furniture.
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="container-fluid text-center">
-			<div class="pbg">
-				<img src="~/assets/img/products.png" alt="" />
-			</div>
-		</div>
 		<div v-if="error">{{ error.message }}</div>
-		<div v-else-if="hasSearchParam && !filteredProducts.length">
-			Sonuç Bulunamadı
-		</div>
-		<div v-else-if="!filteredProducts.length">Loading...</div>
+		<div v-else-if="!product">Loading...</div>
 		<div v-else>
-			<div class="container">
+			<div class="container-fluid section">
 				<div class="row">
-					<div
-						v-for="product in filteredProducts"
-						:key="product.id"
-						class="col-md-4 mb-4"
-					>
-						<div class="product-card">
-							<nuxt-link :to="'/products/' + product.id">
-								<img
-									:src="getFullImagePath(product.image2)"
-									alt="Product Image"
-									class="img-fluid mb-3"
-								/>
-								<div class="product-info">
-									<h5 class="brand-name">
-										{{ product.brand }}
-										<span>{{ product.name }}</span>
-									</h5>
-									<div class="product-actions text-center">
-										<nuxt-link
-											:to="'/products/' + product.id"
-											class="readmore"
-											>Read More</nuxt-link
-										>
-										<nuxt-link
-											:to="
-												'/products/' +
-												product.id +
-												'/buy'
-											"
-											class="buynow"
-											>Buy Now</nuxt-link
-										>
+					<div class="col-md-6 details">
+						<div class="row">
+							<div class="col-md-6">
+								<h1 class="pbrand">{{ product.brand }}</h1>
+								<h1 class="ptitle">{{ product.name }}</h1>
+								<div class="row">
+									<div class="col-md-3">
+										<h3 class="psize">TWO SIZE</h3>
+									</div>
+									<div class="col-md-8 pl0">
+										<div class="sizes">
+											<a
+												href="#"
+												:class="{
+													active:
+														activeSize === '1.3L',
+												}"
+												@click="
+													changeProductSize('1.3L')
+												"
+												>1.3L</a
+											>
+											<a
+												href="#"
+												:class="{
+													active:
+														activeSize === '360ML',
+												}"
+												@click="
+													changeProductSize('360ML')
+												"
+												>360ML</a
+											>
+										</div>
 									</div>
 								</div>
-							</nuxt-link>
+
+								<div class="categories row">
+									<h1 class="pftitle">CATEGORIES</h1>
+									<p class="p0">Wood Oil</p>
+								</div>
+
+								<div class="colors row">
+									<h1 class="pftitle clrs">COLOURS</h1>
+									<div class="col-md-12">
+										<SelectedColors />
+									</div>
+								</div>
+							</div>
+
+							<div class="col-md-6">
+								<div class="breadcump">
+									<a href="#">Products</a> <Right />
+									<a href="#">Natura Onecoat Wood Oil</a>
+									<Right />
+									<a href="#">Clear</a>
+								</div>
+								<p class="firstdetails">
+									Natura Onecoat is a hardwax wood oil that
+									creates bonds among the upper fibers of the
+									wood without creating a layer on the
+									surface.
+								</p>
+								<div class="accordion">
+									<h1
+										class="accordiontitle"
+										@click="toggleDetails"
+									>
+										Details {{ showDetails ? '-' : '+' }}
+									</h1>
+									<div
+										v-if="showDetails"
+										class="details-content"
+									>
+										<!-- <p v-html="product.desc"></p> -->
+										<div class="msds-buttons">
+											<button>MSDS 2K-A</button>
+											<button class="bl">
+												MSDS 2K-B
+											</button>
+										</div>
+									</div>
+								</div>
+								<button class="buy-now">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="18.972"
+										class="cartsvg"
+										height="19.003"
+										viewBox="0 0 18.972 19.003"
+									>
+										<g
+											id="cart_shop_buy_retail"
+											data-name="cart, shop, buy, retail"
+											transform="translate(-3 -3)"
+										>
+											<path
+												id="Path_112873"
+												data-name="Path 112873"
+												d="M23.189,23a2.189,2.189,0,1,0,2.189,2.189A2.189,2.189,0,0,0,23.189,23Zm0,2.919a.73.73,0,1,1,.73-.73A.73.73,0,0,1,23.189,25.919Z"
+												transform="translate(-4.865 -5.406)"
+												fill="#fff"
+											/>
+											<path
+												id="Path_112874"
+												data-name="Path 112874"
+												d="M13.946,17.594H8.108a.73.73,0,0,1,0-1.459H19.783a.73.73,0,0,0,.72-.61l1.459-8.756a.73.73,0,0,0-.72-.85H9.567a.73.73,0,0,0,0,1.459H20.381l-1.216,7.3H10.073l-4.2-11.2A.73.73,0,0,0,5.189,3H3.73a.73.73,0,0,0,0,1.459h.954L8.514,14.675H8.108a2.182,2.182,0,0,0-.569,4.292,2.193,2.193,0,1,0,4.083.086h2.324a.73.73,0,0,0,0-1.459ZM10.3,19.783a.73.73,0,1,1-.73-.73.73.73,0,0,1,.73.73Z"
+												transform="translate(0 0)"
+												fill="#fff"
+											/>
+										</g>
+									</svg>
+									BUY NOW
+								</button>
+							</div>
 						</div>
+						<div class="row size-coverage">
+							<div class="col-md-6">
+								<h1 class="pftitle">
+									SIZE
+
+									<p class="pfdetail">12.17 oz.(1.3L)</p>
+								</h1>
+							</div>
+							<div class="col-md-6">
+								<h1 class="pftitle">
+									COVERAGE AREA
+									<p class="pfdetail">
+										120-190 sq. ft. surface.
+									</p>
+								</h1>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 productarea">
+						<img :src="currentImage" alt="" />
 					</div>
 				</div>
 			</div>
@@ -69,134 +149,189 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed, watch } from 'vue'
-import { fetchProducts } from '@/services/directus'
-import { useRuntimeConfig, useRoute } from '#app'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
 
-const products = ref([])
-const filteredProducts = ref([])
-const error = ref(null)
-const config = useRuntimeConfig()
 const route = useRoute()
+const product = ref(null)
+const currentImage = ref('')
+const activeSize = ref('1.3L')
+const error = ref(null)
+const directusBaseUrl = useRuntimeConfig().public.directusApiUrl
+const showDetails = ref(false)
 
-const hasSearchParam = ref(false)
-
-const getFullImagePath = (imagePath) => {
-	return `${config.public.directusApiUrl}/assets/${imagePath}`
-}
-
-const filterProducts = (searchTerm) => {
-	if (!searchTerm) {
-		filteredProducts.value = products.value
-	} else {
-		const lowerCaseSearchTerm = searchTerm?.toLowerCase() || ''
-		filteredProducts.value = products.value.filter((product) =>
-			product.name?.toLowerCase().includes(lowerCaseSearchTerm)
-		)
-	}
-}
-
-// Watch for changes in the route query to set hasSearchParam and filter products
-watch(
-	() => route.query.search,
-	(newVal) => {
-		hasSearchParam.value = !!newVal
-		filterProducts(newVal)
-	},
-	{ immediate: true }
-)
-
-onMounted(async () => {
+const fetchProduct = async (productId) => {
 	try {
-		products.value = await fetchProducts()
-		filterProducts(route.query.search)
+		const { data } = await axios.get(
+			`${directusBaseUrl}/items/Products/${productId}`
+		)
+		product.value = data.data
+
+		if (product.value.image) {
+			currentImage.value = `${directusBaseUrl}/assets/${product.value.image}`
+		} else {
+			currentImage.value = '~/assets/img/naturaproduct.png'
+		}
 	} catch (err) {
 		error.value = err
-		console.error('Fetch products error:', err)
+		console.error('Product fetch error:', err)
 	}
+}
+
+const changeProductSize = (size) => {
+	activeSize.value = size
+	if (size === '1.3L') {
+		currentImage.value = `${directusBaseUrl}/assets/${product.value.image}`
+	} else if (size === '360ML') {
+		currentImage.value = `${directusBaseUrl}/assets/${product.value.image2}`
+	}
+}
+
+const toggleDetails = () => {
+	showDetails.value = !showDetails.value
+}
+
+onMounted(async () => {
+	await fetchProduct(route.params.id)
 })
 
 definePageMeta({
 	layout: 'header2',
 })
 </script>
-<style scoped>
-.readmore {
-	text-decoration: underline;
-	font-family: 'Avenir Roman';
-	font-size: 12px;
-	color: #000000;
-	letter-spacing: 0px;
+
+<style>
+.color-selectbox {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 }
-.buynow {
-	text-decoration: underline;
-	font-family: 'Avenir Roman';
-	font-size: 12px;
-	letter-spacing: 0px;
-	color: #1d707f;
-	margin-left: 15px;
+
+.color-selectbox select {
+	width: 60%;
+	padding: 10px;
+	font-size: 16px;
 }
-a {
+
+.selected-image img {
+	width: 100px;
+	height: 100px;
+	object-fit: cover;
+	margin-left: 20px;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+}
+
+.clrs {
+	margin-top: 77px;
+	padding-right: 55px;
+	padding-left: 25px;
+}
+
+.p0 {
+	padding: 0;
+	color: black;
+	font-size: 21px;
+	font-family: 'Avenir Roman';
+}
+.pl0 {
+	padding-left: 0;
+}
+.firstdetails {
+	margin-top: 0px;
+}
+.cartsvg {
+	margin-top: -4px;
+	margin-right: 10px;
+}
+.breadcump {
+	font-family: 'Avenir Roman';
 	text-decoration: none;
+	font-size: 12px;
+	letter-spacing: 1.2px;
+	color: #696d6e;
 }
-.brand-name {
+.breadcump a {
+	font-family: 'Avenir Roman';
+	text-decoration: none;
+	font-size: 12px;
+	letter-spacing: 1.2px;
+	color: #696d6e;
+}
+.accordiontitle {
+	font-family: 'Avenir Roman';
+
+	font-size: 13px;
+	letter-spacing: 2.6px;
+	color: #000000;
+}
+.pftitle {
+	font-family: 'Avenir Roman';
+	font-size: 13px;
+	letter-spacing: 2.6px;
+	color: #000000;
+	padding: 0;
+}
+.pfdetail {
+	font-size: 21px;
+
+	margin: 16px 0 0 0;
+	letter-spacing: 0px;
+	color: #000000;
+	font-family: 'Avenir Roman';
+}
+.details {
+	padding-top: 200px;
+	padding-left: 46px;
+}
+.productarea {
+	background: #f5f5f5;
 	text-align: center;
-	color: #000000;
-	font-size: 20px;
-	text-decoration: none;
+	padding-top: 200px;
+}
+.psize {
+	font-family: 'Avenir Roman';
+	color: #696d6e;
+	font-size: 16px;
+	margin-bottom: 200px;
+	padding-top: 6px;
+}
+.pbrand {
 	font-family: 'Helvetica Neue';
+	font-size: 37px;
+	font: normal normal 100 37px/45px Helvetica Neue;
+	letter-spacing: -2.22px;
+	color: #000000;
+	margin-bottom: -2px;
 }
-.brand-name span {
-	font-weight: bold;
-	text-shadow: 0px 0px 0px black;
-}
-.of {
-	font-family: 'Rionaldo Angelo';
-	font-size: 1000px;
-	color: #e4e4e4;
-	position: relative;
-	top: 80px;
-	z-index: -1;
-	left: 13px;
-	margin: 0 50px;
-}
-.pbg {
-	position: relative;
-	margin-top: 70px;
-	margin-bottom: 70px;
-}
-.pbg:before {
-	content: '';
-	position: absolute;
-	height: 180px;
-	left: 0;
-	z-index: -1;
-	width: 100%;
-	top: 205px;
-	background: #232631;
-}
-.headtitle {
-	text-align: center;
-	margin-top: 130px;
-	position: relative;
-}
-.headtitle h1 {
-	font-family: 'Anton', sans-serif;
-	font-size: 140px;
+.ptitle {
+	font-size: 37px;
 	font-weight: 400;
-	color: #232631;
-	font-style: normal;
-	margin-top: -3.2em;
-	margin-bottom: -2.7em;
-	position: relative;
-	z-index: -1;
+	font-size: 37px;
+	letter-spacing: -2.22px;
 }
-.headtitle p {
-	color: #2c2c2c;
+
+.sizes a {
+	background: #f7f7f8 0% 0% no-repeat padding-box;
+	border-radius: 2px;
+	font-weight: bold;
 	font-family: 'Avenir Roman';
-	text-align: center;
-	letter-spacing: 1.67px;
-	font-size: 26px;
+	font-size: 10px;
+	color: #696d6e;
+	padding: 4px 21px;
+	text-decoration: none;
+}
+.sizes a.active {
+	background: #175e6c 0% 0% no-repeat padding-box;
+	border-radius: 2px;
+	font-weight: bold;
+	font-family: 'Avenir Roman';
+	font-size: 10px;
+	color: white;
+	padding: 4px 21px;
+
+	text-decoration: none;
 }
 .accordion h1 {
 	cursor: pointer;
@@ -205,14 +340,8 @@ a {
 	border-bottom: 1px solid black;
 	padding: 20px 0;
 }
-.accordion .details-content {
-	overflow: hidden;
-	max-height: 0;
-	transition: max-height 0.5s ease-out;
-}
-.accordion .details-content.open {
-	max-height: 1000px;
-	transition: max-height 0.5s ease-in;
+.details-content {
+	margin-top: 10px;
 }
 .size-coverage {
 	margin-top: 20px;
